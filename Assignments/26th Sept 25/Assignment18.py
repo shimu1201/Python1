a5cb4471cce5a7d0893c829be0268513b1fc0b2e
shimu1201 <shimunshah121@gmail.com>
2025-10-04 10:28:44 +0530

    modified:   Assignments/26th Sept 25/Assignment18.py

--- Assignments/26th Sept 25/Assignment18.py
+++ Assignments/26th Sept 25/Assignment18.py
@@ -1,13 +1,1 @@
 rates = {"sedan" : 1000, "suv": 1500, "hatchback": 800}
-
-car_type = input("Enter the car type :")
-days = int(input("Enter number of days : "))
-
-if car_type in rates:
-    total = rates[car_type] * days
-    if days > 5:
-        discount = total * 0.10
-        total -= discount
-        print(f"Total rent for {days} days is : {total}")   
-else:
-    print("Invalid Car Type")
